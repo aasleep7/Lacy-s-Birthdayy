bdc79fae6b857f05125bf725e8b5e724893de485
aasleep7 <shiva07ramroop19065@gmail.com>
2026-02-05 22:03:47 -0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const heart = document.getElementById("heart");
-
-heart.addEventListener("click", () => {
-  heart.style.transform = "rotate(-45deg) scale(1.2)";
-  setTimeout(() => {
-    heart.style.transform = "rotate(-45deg) scale(1)";
-  }, 300);
-});
